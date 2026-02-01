c1e44eb65c7a2db54475be3a59725c8917c10b6a
Shikhar Tiwari <tshikhar11@gmail.com>
2026-02-02 01:53:35 +0530

    Day 1 :Python Basics

---
+++ day1_python_basics.py
@@ -0,0 +1,17 @@
+# name = input("enter your name :")
+# age =int(input("Enter your age :"))
+# print(f"Your name is {name} and your age is {age}")
+
+#factorization 
+n = 5
+fact = 1
+i = 1
+while i <= n:
+
+    fact = fact * i
+    i+= 1
+
+print(fact)
+
+
+
